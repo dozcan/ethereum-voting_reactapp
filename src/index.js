310f50a1d32a8cb107e9c5538cd9220aa5242fbd
dozcan <dogasava@gmail.com>
2020-03-16 00:47:05 +0300

    react hook ethereum hook frontend

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { StoreProvider } from "./store";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <StoreProvider>
+      <App />
+    </StoreProvider>
+  </React.StrictMode>,
+  rootElement
+);
