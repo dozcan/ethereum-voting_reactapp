dd0c957f22de7d4f0463e8a29f627dc6fa0ea458
doga ozcan <dogasava@gmail.com>
2021-03-14 20:45:50 +0300

    Update deployment.js

--- src/components/deployment.js
+++ src/components/deployment.js
@@ -7,11 +7,44 @@ import { store } from "../redux/store";
 export const DeploySmartContract = () => {
   const [hide, setHide] = useState(false);
   const { state, dispatch } = useContext(store);
-  const deploySmartContract = () => {
+  const deploySmartContract = async() => {
     setHide(true);
+    const { ethereum } = window;
+    ethereum.on('accountsChanged', (chainId) => {
+      console.log("accountsChanged",chainId);
+    });
+    if(ethereum.selectedAddress === null){
+      console.log("baglı degiliz")
+    }
+    else{
+      console.log("baglıyız")
+    }
+    await ethereum.request({ method: 'eth_requestAccounts' });
+    console.log("ethereum.selectedAddress",ethereum.selectedAddress,ethereum._state.isConnected);
+    if(ethereum.selectedAddress === null){
+      console.log("baglı degiliz")
+    }
+    else{
+      console.log("baglıyız")
+    }
+ 
+    const transactionHash = await ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          to: '0x177003D21079DaC6104A147b4Edb698b3D27cB11',
+          from: ethereum.selectedAddress,
+          value: '1',
+        },
+      ],
+    });
+    // Handle the result
+    console.log(transactionHash);
+
     dispatch({ type: "Contract", payload: "Ox123456678798909090" });
   };
 
+  
   return (
     <div>
       <Card>
